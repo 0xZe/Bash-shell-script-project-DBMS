7186051eff0bff40a112d5b1ebbcaf1c2d448ef9
mohamed eldeeb <deeb652000@gmail.com>
2023-04-08 19:49:06 +0200

    update update_table

--- update_table.sh
+++ update_table.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
-#enter column 
-echo "Enter column you want to update: "
-read col_update
-#checkcol=`awk -v RS=':' "/$colUpdate/ "'{print NR}' mydb/.metadata/mytb.meta`
-col_NR=`awk -v RS=':' "/$col_update/ "'{print NR}' ./.metadata/mytb.meta`
-#echo $updateNum
-#echo "jjjj"
-#fieldNumber=
-#searchResult=
-#if [ -z $checkcol ]
-#    then
-#    echo "column does not exist"
-#    ./manipulate_db.sh $1
-#else
+db_name=$1
+read  -p  "Enter Table you want to delete: " table_name
+table_path=DataBases/$db_name/$table_name
 
-    #value yoy want to change
+if [ ! -f "$table_path" ]; then
+
+    echo "Table does not exist!!!!!!"
+
+    ./manipulate_db.sh $1
+
+else
+
+    echo "Enter column you want to update: "
+    read col_update
+                                                             
+    check_col=`awk -v RS=':' "/$col_update/ "'{print NR}' ./DataBases/$1/.metadata/$table_name.meta``
+    col_NR=`awk -v RS=':' "/$col_update/ "'{print NR}' ./DataBases/$1/.metadata/$table_name.meta``
+
+    if [ -z $check_col ]
+      then
+      echo "Column does not exist!!!!!"
+
+      ./manipulate_db.sh $1
+
+    else
+
+    #value you want to change
     echo "Enter $col_update value you want to change: "
     read value_update
-    #line number
-    value_NR=`cut -d: -f $col_NR mytb 2>/dev/null | awk "/$value_update/ "'{print NR}' `
-    #echo $searchResult 
 
-    #echo $updateNum
+    
+    value_NR=`cut -d: -f $col_NR mytb 2>/dev/null | awk "/$value_update/ "'{print NR}' `
+    
 
     echo "Enter new $value_update value"
     read new_value
@@ -30,13 +40,12 @@ col_NR=`awk -v RS=':' "/$col_update/ "'{print NR}' ./.metadata/mytb.meta`
     if(NR=="'$value_NR'"){
          print $'$col_NR';     
          }
-      }' mytb)
+      }' ./DataBases/$1/$table_name)
 
     sed -i ''$value_NR's/'$old_value'/'$new_value'/g' mytb 2>>/dev/null
 
-    #sed -i 's/STRING_TO_REPLACE/STRING_TO_REPLACE_IT/g' filename
+    echo "Updated $col_update successfully !!!"
 
-    echo "Updated $col_update successfully !!"
-
+    fi
     
-#fi
+fi
