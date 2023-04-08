57ea1d1f4ae39871989ff78c6876a6db52a4d5d4
Mohammed Eldeeb <81789671+0xZe@users.noreply.github.com>
2023-04-09 00:37:41 +0200

    Update delete_table.sh

--- delete_table.sh
+++ delete_table.sh
@@ -9,8 +9,29 @@ if [ ! -f "$table_path" ]; then
     ./manipulate_db.sh $1
 else
     # delete table Content
-    rm -ir $table_path
-    echo "$table_name was deleted Sucessfully!!!!"
-
+    #rm -ir $table_path
+    #echo "$table_name was deleted Sucessfully!!!!"
+    echo -e "Are you Sure You Want To delete $table_name Table? y/n \c"
+	read choice;
+	case $choice in
+		 [Yy]* ) 
+			rm -r $table_path
+                echo "============================================"
+         		echo "Table $table_name deleted successfully!!!!!!"
+         		echo "============================================"
+			;;
+		 [Nn]* ) 
+      			echo "======================"
+			    echo "Deleting Canceled!!!"
+      			echo "======================"
+			;;
+		 * ) 
+		 	echo "===================="
+			echo "Please choice y/n"
+			echo "===================="
+			;;
+	esac
+  
     ./manipulate_db.sh $1
+
 fi
