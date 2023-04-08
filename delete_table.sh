51d321239ddac92731995fc00ca7f7c6ae009719
Mohammed Eldeeb <81789671+0xZe@users.noreply.github.com>
2023-04-09 00:39:20 +0200

    Update delete_table.sh

--- delete_table.sh
+++ delete_table.sh
@@ -4,25 +4,30 @@ read  -p  "Enter Table you want to delete: " table_name
 table_path=DataBases/$db_name/$table_name
 
 if [ ! -f "$table_path" ]; then
+
+    echo "==========================="	
     echo "Table does not exist!!!!!!"
+    echo "==========================="
 
     ./manipulate_db.sh $1
 else
+
     # delete table Content
     #rm -ir $table_path
     #echo "$table_name was deleted Sucessfully!!!!"
-    echo -e "Are you Sure You Want To delete $table_name Table? y/n \c"
+    
+   	echo -e "Are you Sure You Want To delete $table_name Table? y/n \c"
 	read choice;
 	case $choice in
 		 [Yy]* ) 
 			rm -r $table_path
-                echo "============================================"
+                	echo "============================================"
          		echo "Table $table_name deleted successfully!!!!!!"
          		echo "============================================"
 			;;
 		 [Nn]* ) 
       			echo "======================"
-			    echo "Deleting Canceled!!!"
+			echo "Deleting Canceled!!!"
       			echo "======================"
 			;;
 		 * ) 
